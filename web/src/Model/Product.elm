670ee0219675f507436d63c6f2c8d9ffa16b50ae
Henrique Buss <henrique.buss@hotmail.com>
2021-01-06 16:04:34 -0300

    Added products page and model

---
+++ web/src/Model/Product.elm
@@ -0,0 +1,69 @@
+module Model.Product exposing (Product, exampleProduct, formatPrice)
+
+-- TYPE
+
+
+type alias Product =
+    { name : String
+    , description : String
+    , price : Float
+    , imageUri : String
+    }
+
+
+
+-- PUBLIC HELPERS
+
+
+formatPrice : Float -> String
+formatPrice price =
+    String.fromFloat price
+        |> addComma
+        |> normalizeZeros
+
+
+
+-- INTERNAL
+
+
+addComma : String -> String
+addComma price =
+    if String.contains "." price then
+        String.replace "." "," price
+
+    else
+        price ++ ","
+
+
+normalizeZeros : String -> String
+normalizeZeros price =
+    let
+        portions =
+            String.split "," price
+    in
+    case ( List.head portions, List.head <| List.drop 1 portions ) of
+        ( Just beforeComma, Just afterComma ) ->
+            if String.length afterComma == 2 then
+                beforeComma ++ "," ++ afterComma
+
+            else if String.length afterComma < 2 then
+                beforeComma ++ "," ++ afterComma ++ String.repeat (2 - String.length afterComma) "0"
+
+            else
+                beforeComma ++ "," ++ String.left 2 afterComma
+
+        _ ->
+            ""
+
+
+
+-- TESTING
+
+
+exampleProduct : Product
+exampleProduct =
+    { name = "Pizza Bacon"
+    , description = "Pizza bacon"
+    , price = 29.9
+    , imageUri = "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg"
+    }
--- web/src/Page/Products.elm
+++ web/src/Page/Products.elm
@@ -2,8 +2,11 @@ module Page.Products exposing (..)
 
 import Colors
 import Element exposing (Element)
+import Element.Background as Background
+import Element.Border as Border
 import Element.Font as Font
 import Element.Region as Region
+import Model.Product as Product exposing (Product)
 
 
 
@@ -14,6 +17,7 @@ view : Element msg
 view =
     Element.column [ Element.width Element.fill, Element.height Element.fill ]
         [ viewHeader
+        , viewProductList <| List.repeat 6 Product.exampleProduct
         ]
 
 
@@ -43,5 +47,49 @@ viewHeader =
                 , Element.el [] <| Element.text "Depois clique em "
                 , Element.el [ Font.bold ] <| Element.text "\"ENVIAR PEDIDO\""
                 ]
+            ]
+        ]
+
+
+viewProductList : List Product -> Element msg
+viewProductList products =
+    Element.wrappedRow
+        [ Element.height Element.fill
+        , Element.width Element.fill
+        , Background.color Colors.light
+        , Element.spacing 20
+        , Element.paddingXY 100 20
+        ]
+    <|
+        List.map viewProductCard products
+
+
+viewProductCard : Product -> Element msg
+viewProductCard product =
+    Element.column
+        [ Background.color <| Element.rgb 1 1 1
+        , Border.rounded 10
+        , Element.padding 20
+        , Element.spacing 20
+        , Border.shadow { offset = ( 0, 4 ), size = 0, blur = 20, color = Element.rgba 0 0 0 0.25 }
+        , Font.color Colors.secondary
+        , Element.mouseOver [ Element.scale 1.01 ]
+        ]
+        [ Element.el
+            [ Font.center
+            , Element.width Element.fill
+            , Font.color Colors.primary
+            , Font.bold
+            , Font.size 18
+            ]
+          <|
+            Element.text product.name
+        , Element.image [ Border.rounded 10, Element.clip, Element.centerX, Element.centerY, Element.width <| Element.px 220 ] { src = product.imageUri, description = product.name }
+        , Element.el [ Font.bold, Font.color Colors.primary, Font.size 24 ] <|
+            Element.text ("R$ " ++ Product.formatPrice product.price)
+        , Element.el [ Element.width Element.fill, Element.height <| Element.px 2, Background.color <| Element.rgb255 0xE6 0xE6 0xE6 ] Element.none
+        , Element.column [ Element.spacing 30 ]
+            [ Element.el [ Font.bold, Font.size 16 ] <| Element.text "Descrição"
+            , Element.el [ Font.size 14 ] <| Element.text product.description
             ]
         ]
