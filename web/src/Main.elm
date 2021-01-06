7f599995dd13ffc3f9673a9071640b6fa6a72ea7
Henrique Buss <henrique.buss@hotmail.com>
2021-01-06 11:41:28 -0300

    Added navbar

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
+import Element exposing (Element)
+import Element.Background as Background
+import Element.Font as Font
+import Element.Region as Region
+import Html exposing (Html)
 
 
 
@@ -45,7 +49,56 @@ main =
 
 view : Model -> Html Msg
 view model =
-    text "Hello World"
+    Element.layout [ Font.family [ Font.typeface "Open Sans", Font.sansSerif ] ] <|
+        Element.column [ Element.width Element.fill, Element.height Element.fill ]
+            [ viewNavbar
+            ]
+
+
+primaryColor : Element.Color
+primaryColor =
+    Element.rgb255 0xDA 0x5C 0x5C
+
+
+primaryHoverColor : Element.Color
+primaryHoverColor =
+    Element.rgb255 0xA7 0x4B 0x4B
+
+
+darkColor : Element.Color
+darkColor =
+    Element.rgb255 0x26 0x32 0x38
+
+
+secondaryColor : Element.Color
+secondaryColor =
+    Element.rgb255 0x9E 0x9E 0x9E
+
+
+lightColor : Element.Color
+lightColor =
+    Element.rgb255 0xF5 0xF5 0xF5
+
+
+viewNavbar : Element msg
+viewNavbar =
+    Element.row
+        [ Region.navigation
+        , Background.color primaryColor
+        , Element.height <| Element.px 70
+        , Element.width Element.fill
+        , Element.paddingXY 45 0
+        , Element.spacing 15
+        ]
+        [ Element.image [ Element.centerY ]
+            { src = "/assets/logo.svg", description = "" }
+        , Element.link
+            [ Font.bold
+            , Font.color <| Element.rgb 1 1 1
+            , Font.size 24
+            ]
+            { url = "/", label = Element.text "DS Deliver" }
+        ]
 
 
 
