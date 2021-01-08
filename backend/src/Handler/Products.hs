d11e420693b29f88d3cdf12cb469e9d0b37f1039
Henrique Buss <henrique.buss@hotmail.com>
2021-01-08 10:54:30 -0300

    Removed haskell backend

--- backend/src/Handler/Products.hs
+++
@@ -1,8 +0,0 @@
-module Handler.Products where
-
-import Import
-
-getProductsR :: Handler TypedContent
-getProductsR = do
-  products <- runDB $ selectList [] [Asc ProductName]
-  selectRep $ provideJson products
