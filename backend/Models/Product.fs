8418bf53fd217322db32fa077810cb7616aaadce
Henrique Buss <henrique.buss@hotmail.com>
2021-01-11 10:08:05 -0300

    Removed F# backend

--- backend/Models/Product.fs
+++
@@ -1,8 +0,0 @@
-namespace Backend.Models
-
-type Product =
-    { Id: int64
-      Name: string
-      Price: double
-      Description: string
-      ImageUri: string }
--- backend/scripts/prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-touch ./.env
-source ./.env
-
-echo "module Backend.Env
-
-[<Literal>]
-let BackendUrl = \"$BACKEND_URL\"" > Env.fs
